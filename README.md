1128a8ac1a23e40aa37c9064a9f10eb64d6a40ce
Sam Fry <benolot@gmail.com>
2018-01-02 00:23:39 +0000

    Add positions via the library, not manually

--- README.md
+++ README.md
@@ -11,17 +11,24 @@ Take it back now y'all
  - [ ] Documentation
  - [ ] Implement API To library
  - [ ] Object Orientated
- - [ ] Set the position element via the JS and not manually
  - [ ] Add the controls via the JS and not manually
  - [ ] Remove gap between slide contents and slide label
  - [ ] Cleanup the code
  - [ ] Add mobile support for default theme
+ - [ ] Turn features (Progress, auto scroll, etc) on and off via customisation
+ - [ ] Initial function to run when page is loaded that setups library
  - [x] Rename CSS Classes to be Cha Cha Slider specific
  - [x] Add slideBack() function
  - [x] Add slideForward() function
  - [x] Add jumpSlide() function
  - [x] Add custom text field to top right to allow for copyright info, etc
+ - [x] Set the position element via the JS and not manually
 
 # Version/Changelog
 
-_Currently in version 0.0.1 no changelog can be provided_
+**__Version 0.0.2__**
+- Added the slide count & current slide into top left corner
+
+**__Version 0.0.1__**
+- Launched
+
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,7 @@
 var slideIndex = 1;
+addPositions();
 showSlide(slideIndex);
+// Should really move all of the above into an intilisation function that is called on load
 
 function jumpSlide(n) {
     showSlide(n);
@@ -7,7 +9,7 @@ function jumpSlide(n) {
 
 // Named this silly name due to the cha cha slide song
 function slideToTheRight() {
-    var n = slideIndex+= 1;
+    var n = slideIndex+= 1; // Using var here for browser support, rather than using let or similar.
     var slides = document.getElementsByClassName("cc-slider_item");
     if (n > slides.length) {
         slideIndex = 1;
@@ -50,6 +52,14 @@ function showSlide(n) {
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" cc-slider_active", "");
     }
-    slides[slideIndex-1].style.display = "block"; 
+    slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " cc-slider_active";
+}
+
+function addPositions() {
+    var slides = document.getElementsByClassName("cc-slider_item");
+    var slide_count = slides.length;
+    for (var i = 0; i < slide_count; i++) {
+        slides[i].innerHTML += ('<div class="cc-slider_position">' + (i + 1) + '/' + slide_count  + '</div>');
+    }
 }
